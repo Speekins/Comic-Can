fd3f48c098ee27ed1cfe48329f63ef474e1fabfe
Eleanor Ruth <ellie.ruth@me.com>
2023-01-06 16:44:18 -0700

    Add flexbox to styling

--- src/components/ComicDetail/ComicDetail.js
+++ src/components/ComicDetail/ComicDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router";
+import './ComicDetail.css'
 
 const ComicDetail = ({ findCards }) => {
   const { id } = useParams();
@@ -8,13 +9,15 @@ const ComicDetail = ({ findCards }) => {
 
   return (
     <div className="cardDetails">
-      <p>Title: {card.title}</p>
-      <p>Year: {card.year}</p>
-      <p>Issue: {card.issue}</p>
-      <p>Grade: {card.grade}</p>
       <img src={card.image_path} alt={`Cover of ${card.title}`} />
-      <p>Verified: {card.verified}</p>
-      <p>Notes: {card.notes}</p>
+      <div className="cardInfo">
+        <p>Title: {card.title}</p>
+        <p>Year: {card.year}</p>
+        <p>Issue: {card.issue}</p>
+        <p>Grade: {card.grade}</p>
+        <p>Verified: {card.verified}</p>
+        <p>Notes: {card.note}</p>
+      </div>
     </div>
   );
 };
