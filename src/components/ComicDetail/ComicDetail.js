6b6a10af76d34c27b7a9a4e415371002c37514fa
Eleanor Ruth <ellie.ruth@me.com>
2023-01-06 17:07:42 -0700

    conditionally render verified

--- src/components/ComicDetail/ComicDetail.js
+++ src/components/ComicDetail/ComicDetail.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { useParams } from "react-router";
 import './ComicDetail.css'
+import verifiedImage from '../assets/verified.png'
 
 const ComicDetail = ({ findCards }) => {
   const { id } = useParams();
 
   const card = findCards(id);
-
+  // const checkVerification = () => {
+  //   var isTrueSet = (card.verified.toLowerCase() === 'true')
+  //   if (isTrueSet) {
+  //     return <img src={verifiedImage} alt={}
+  //   }
+  }
   return (
     <div className="cardDetails">
       <img src={card.image_path} alt={`Cover of ${card.title}`} />
@@ -15,7 +21,7 @@ const ComicDetail = ({ findCards }) => {
         <p>Year: {card.year}</p>
         <p>Issue: {card.issue}</p>
         <p>Grade: {card.grade}</p>
-        <p>Verified: {card.verified}</p>
+        {/* <p>Verified: {}</p> */}
         <p>Notes: {card.note}</p>
       </div>
     </div>
